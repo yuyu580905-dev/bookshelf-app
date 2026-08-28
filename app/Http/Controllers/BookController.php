ac4d8828d96ceea22d86db0314f5430c2c03ffad
matsumoto yusuke <yuyu580905@yahoo.co.jp>
2026-08-28 16:16:07 +0900

    feat: 書籍一覧表示のためにルートを追加しControllerを作成

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Book;
 
-class BooksController extends Controller
+class BookController extends Controller
 {
     public function index()
     {
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,19 @@
 <?php
 
-use App\Http\Controllers\BooksController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\FavoriteController;
+use App\Http\Controllers\GenreController;
+use App\Http\Controllers\RankingController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [BooksController::class, 'index'])->name('books.index');
-Route::get('/books/{book}', [BooksController::class, 'show'])->name('books.show');
-Route::get('/books/create', [BooksController::class, 'create'])->name('books.create');
+Route::get('/', [BookController::class, 'index'])->name('books.index');
+Route::get('/books/{book}', [BookController::class, 'show'])->name('books.show');
+Route::get('/books/create', [BookController::class, 'create'])->name('books.create');
+Route::get('/ranking', [RankingController::class, 'index'])->name('ranking.index');
+Route::get('/favorites', [FavoriteController::class, 'index'])->name('favorites.index');
+Route::post('/favorites/{book}/toggle', [FavoriteController::class, 'toggle'])->name('favorites.toggle');
+Route::get('/genres', [GenreController::class, 'index'])->name('genres.index');
+Route::get('/genres/create', [GenreController::class, 'create'])->name('genres.create');
+Route::get('/genres/{genre}', [GenreController::class, 'show'])->name('genres.show');
+Route::get('/genres/{genre}/edit', [GenreController::class, 'edit'])->name('genres.edit');
+Route::delete('/genres/{genre}', [GenreController::class, 'destroy'])->name('genres.destroy');
